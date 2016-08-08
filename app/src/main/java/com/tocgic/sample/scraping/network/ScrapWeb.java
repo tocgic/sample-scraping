a566622c95ea2bc8a46124114939cceb89513903
tocgic <jeongyon.cho@gmail.com>
2016-08-08 14:18:16 +0900

    jsoup의 select 정규식 활용

--- app/src/main/java/com/tocgic/sample/scraping/network/ScrapWeb.java
+++ app/src/main/java/com/tocgic/sample/scraping/network/ScrapWeb.java
@@ -50,53 +50,28 @@ public class ScrapWeb {
         if (url != null && url.length() > 0) {
             try {
                 Document doc = Jsoup.parse(new URL(url).openStream(), "euc-kr", url);
-                Elements datas = doc.select("img");
-                for (Element data : datas) {
-                    String src = data.attr("src");
-                    String clazz = data.attr("class");
-                    if (StringUtil.isNotNull(src)) {
-                        if (StringUtil.isNotNull(clazz) && clazz.equalsIgnoreCase("picture")) {
-                            Photo photo = new Photo();
-                            photo.setPath(src);
-                            photoList.add(photo);
-                        }
-                    }
-                }
-
-//                Elements datas = doc.select(".picture");
+//                Elements datas = doc.select("img");
 //                for (Element data : datas) {
 //                    String src = data.attr("src");
-//                    if (StringUtil.isNotNull(src)) {
-//                        Photo photo = new Photo();
-//                        photo.setPath(src);
-//                        photoList.add(photo);
-//                    }
-//                }
-
-//                Elements datas = doc.select("div");
-//                for (Element data : datas) {
 //                    String clazz = data.attr("class");
-//                    if (StringUtil.isNotNull(clazz) && clazz.equalsIgnoreCase("gallery-item-group exitemrepeater")) {
-//                        Elements subImg = data.select("img");
-//                        String src = null;
-//                        if (subImg != null && subImg.size() > 0) {
-//                            src = subImg.get(0).attr("src");
-//                        }
-//                        Elements subA = data.select(".gallery-item-caption a");
-//                        String text = null;
-//                        if (subA != null && subA.size() > 0) {
-//                            src = subA.get(0).data();
-//                        }
-//                        if (StringUtil.isNotNull(src)) {
+//                    if (StringUtil.isNotNull(src)) {
+//                        if (StringUtil.isNotNull(clazz) && clazz.equalsIgnoreCase("picture")) {
 //                            Photo photo = new Photo();
 //                            photo.setPath(src);
-//                            if (StringUtil.isNotNull(text)) {
-//                                photo.setFileName(text);
-//                            }
 //                            photoList.add(photo);
 //                        }
 //                    }
 //                }
+
+                Elements datas = doc.select("img[class=picture]");
+                for (Element data : datas) {
+                    String src = data.attr("src");
+                    if (StringUtil.isNotNull(src)) {
+                        Photo photo = new Photo();
+                        photo.setPath(src);
+                        photoList.add(photo);
+                    }
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
